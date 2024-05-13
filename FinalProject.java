9ca60e4f8036f96ee82c2843d7c9171b35586abe
ssaby5650 <asdfghm@>
2024-05-13 11:39:47 -0700

    I worked on the start screen

--- FinalProject.java
+++ FinalProject.java
@@ -4,16 +4,44 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
 import java.util.ArrayList;
 
 public class FinalProject extends JFrame implements ActionListener, MouseListener{
-    static JFrame gameWindow;
+    JFrame gameWindow;
+    JPanel titleName;
+    JLabel titleNameLabel;
+    Font engraversOldEnglish;
 
     public static void main(String[] args) {
+
+    new FinalProject();
+
+    }
+
+    public FinalProject(){
+        engraversOldEnglish = Font.createFont(Font.TRUETYPE_FONT, new File("OPTIEngraversOldEnglish.otf")).deriveFont(30f);
+
+        //Window
         gameWindow = new JFrame("Dungeon");
         gameWindow.setSize(1000,1000);
-        gameWindow.show();
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameWindow.getContentPane().setBackground(Color.BLACK);
+        gameWindow.setVisible(true);
+        //Title
+        titleName = new JPanel();
+        titleName.setBounds(100, 100, 600, 500);
+        titleName.setBackground(Color.BLACK);
+        titleNameLabel = new JLabel("Matrix");
+        titleNameLabel.setForeground(Color.WHITE);
+        titleNameLabel.setFont(engraversOldEnglish);
+
+
+
     }
+
+
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
