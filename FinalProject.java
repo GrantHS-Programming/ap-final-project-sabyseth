a6a5988b43578f0e5c31ce3f842473238c6b2806
jaslakson1898 <jaslakson1898@student.pps.net>
2024-05-13 10:48:33 -0700

    update

---
+++ FinalProject.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+public class FinalProject extends JFrame implements ActionListener, MouseListener{
+    static JFrame gameWindow;
+
+    public static void main(String[] args) {
+        gameWindow = new JFrame("Dungeon");
+        gameWindow.setSize(1000,1000);
+        gameWindow.show();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
