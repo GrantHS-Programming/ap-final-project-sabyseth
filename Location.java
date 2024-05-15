fa3db6c812e9da4bb936068128ae94260df29c6f
jaslakson1898 <jaslakson1898@student.pps.net>
2024-05-15 10:18:20 -0700

    update

--- Location.java
+++ Location.java
@@ -3,13 +3,32 @@ public class Location {
     private String option2;
     private String option3;
     private String option4;
-    public Location(String opt1, String opt2, String opt3, String opt4){
+    private String text;
+    public Location(String opt1, String opt2, String opt3, String opt4, String locText){
         option1 = opt1;
         option2 = opt2;
         option3 = opt3;
         option4 = opt4;
+        text = locText;
     }
-    public void option1(){
+    public String getOption1() {
+        return option1;
+    }
 
+    public String getOption2() {
+        return option2;
+    }
+
+    public String getOption3() {
+        return option3;
+    }
+
+    public String getOption4() {
+        return option4;
+    }
+
+    @Override
+    public String toString() {
+        return text;
     }
 }
