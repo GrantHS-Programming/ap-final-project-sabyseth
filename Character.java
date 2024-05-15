6d36120b0194fb54bf6510fd74519bf0c40e8347
jaslakson1898 <jaslakson1898@student.pps.net>
2024-05-15 11:37:30 -0700

    update

--- Character.java
+++ Character.java
@@ -3,12 +3,22 @@ import java.util.ArrayList;
 public class Character {
     private int health;
     private String name;
-    private ArrayList<String> inventory;
+    private ArrayList<Item> inventory;
     private int hunger;
-    public Character(){
+    public Character(String named){
         health = 10;
-        name = "Grant Gilmore";
+        name = named;
         inventory = new ArrayList<>();
         hunger = 3;
+    }
+    public void itemAdd(Item item){
+        inventory.add(item);
+    }
+    public Item itemRemove(Item item){
+        for (int x = 0; x < inventory.size(); x++){
+            if (inventory.get(x) == item){
+                Item returnItem = (inventory.remove(item));
+            }
+        }
     }
 }
