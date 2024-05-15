d319517b47bf23da7f5ce501f6bb52b6284d30e7
ssaby5650 <asdfghm@>
2024-05-15 11:26:02 -0700

    Title

--- FinalProject.java
+++ FinalProject.java
@@ -6,7 +6,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+
 
 public class FinalProject extends JFrame implements ActionListener, MouseListener{
     JFrame gameWindow;
@@ -14,55 +14,73 @@ public class FinalProject extends JFrame implements ActionListener, MouseListene
     JLabel titleNameLabel;
     Font engraversOldEnglish;
 
+
     public static void main(String[] args) {
 
-    new FinalProject();
-    Character player = new Character();
-    Location location1 = new Location("Fight", "Heal", "Run", "Inventory", "You wake up...");
-    Location currentLocation = location1;
+
+        new FinalProject();
+        Character player = new Character();
+        Location location1 = new Location("Fight", "Heal", "Run", "Inventory", "You wake up...");
         System.out.println(location1);
         System.out.println(location1.getOption1());
         System.out.println(location1.printAll());
+        Location currentLocation = location1;
+        System.out.println(currentLocation);
+        System.out.println(currentLocation.getOption1());
+        System.out.println(currentLocation.printAll());
+
 
     }
+
 
     public FinalProject(){
         try {
-            engraversOldEnglish = Font.createFont(Font.TRUETYPE_FONT, new File("OPTIEngraversOldEnglish.otf")).deriveFont(20f);
+            engraversOldEnglish = Font.createFont(Font.TRUETYPE_FONT, new File("OPTIEngraversOldEnglish.otf")).deriveFont(200f);
             GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         }
-        catch (IOException | FontFormatException e){
+        catch (IOException | FontFormatException ignored){
+
 
         }
 
 
+        JTextArea text = new JTextArea();
         //Window
-        int width = 1000;
-        int height = 1000;
         gameWindow = new JFrame("Dungeon");
-        gameWindow.setSize(width,height);
+        gameWindow.setSize(1000,1000);
         gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameWindow.getContentPane().setBackground(Color.BLACK);
-        gameWindow.setLayout(null);
 
-        //Title
+
         titleName = new JPanel();
-        titleName.setSize(100, 100);
-        titleName.setLocation(gameWindow.getWidth()/2, gameWindow.getHeight()/2);
+        titleName.setLayout(new GridBagLayout());
+        titleName.add(text);
+
+
         titleName.setBackground(Color.BLACK);
 
-        titleNameLabel = new JLabel("Matrix");
+
+        titleNameLabel = new JLabel("matrix");
         titleNameLabel.setBackground(Color.black);
         titleNameLabel.setForeground(Color.white);
         titleNameLabel.setFont(engraversOldEnglish);
-        titleName.add(titleNameLabel);
 
+
+        titleName.add(titleNameLabel);
         gameWindow.add(titleName);
         gameWindow.setVisible(true);
 
+
+
+
         //Buttons
+        JButton start = new JButton("Begin");
         JButton button1 = new JButton();
     }
+
+
+
+
 
 
 
@@ -70,30 +88,44 @@ public class FinalProject extends JFrame implements ActionListener, MouseListene
     @Override
     public void actionPerformed(ActionEvent e) {
 
+
     }
+
 
     @Override
     public void mouseClicked(MouseEvent e) {
 
+
     }
+
 
     @Override
     public void mousePressed(MouseEvent e) {
 
+
     }
+
 
     @Override
     public void mouseReleased(MouseEvent e) {
 
+
     }
+
 
     @Override
     public void mouseEntered(MouseEvent e) {
 
+
     }
+
 
     @Override
     public void mouseExited(MouseEvent e) {
 
+
     }
 }
+
+
+
